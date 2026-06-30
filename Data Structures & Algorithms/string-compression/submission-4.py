03d02aebad5cfdff9eb412959883ac7303801a84
Nick Chimicles <134860802+nac-codes@users.noreply.github.com>
2026-06-30 16:51:09 -0400

    Add: string-compression - submission-4

---
+++ Data Structures & Algorithms/string-compression/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        write_cursor=1
+        read_cursor=0                
+
+        while read_cursor < len(chars): 
+            base_read_cursor = read_cursor
+
+            while read_cursor < len(chars) and chars[read_cursor] == chars[base_read_cursor]:
+                read_cursor+=1
+            
+            group_size = read_cursor - base_read_cursor
+            if group_size > 1:
+                for digit in str(group_size):
+                    chars[write_cursor] = digit
+                    write_cursor += 1
+                
+            if read_cursor < len(chars):
+                chars[write_cursor] = chars[read_cursor]
+                write_cursor+=1
+               
+
+        return write_cursor
+
+            
+
